3d4ebffeaad269ce74f3ff7a91bb4984ff6a3df7
Sweekriti Satpathy <swsat@microsoft.com>
2022-11-11 15:35:59 -0800

    more ios fixes

--- MyCoffeeApp/MyCoffeeApp/AssemblyInfo.cs
+++ MyCoffeeApp/MyCoffeeApp/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+//[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
-[assembly: ExportFont("CustomFont.ttf", Alias = "AC")]
-[assembly: ExportFont("fa-regular-400.ttf", Alias = "FAR")]
-[assembly: ExportFont("fa-solid-900.ttf", Alias = "FAS")]
-[assembly: ExportFont("fa-brands-400.ttf", Alias = "FAB")]
+//[assembly: ExportFont("CustomFont.ttf", Alias = "AC")]
+//[assembly: ExportFont("fa-regular-400.ttf", Alias = "FAR")]
+//[assembly: ExportFont("fa-solid-900.ttf", Alias = "FAS")]
+//[assembly: ExportFont("fa-brands-400.ttf", Alias = "FAB")]
